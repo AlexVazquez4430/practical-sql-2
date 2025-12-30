12cb305bedccfde0b33848441b2ee59ee669ac03
AlexVazquez4430 <161274923+AlexVazquez4430@users.noreply.github.com>
2025-12-29 21:56:05 -0600

    Aprender sobre table mi_tabla para seleccionar todo

--- Chapter_02/Chapter_02.sql
+++ Chapter_02/Chapter_02.sql
@@ -34,3 +34,8 @@ VALUES ('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200),
        ('Samantha', 'Bush', 'Myers Middle School', '2011-10-30', 36200),
        ('Betty', 'Diaz', 'Myers Middle School', '2005-08-30', 43500),
        ('Kathleen', 'Roush', 'F.D. Roosevelt HS', '2010-10-22', 38500);
+
+select * from teachers where first_name = 'Janet';
+
+
+
